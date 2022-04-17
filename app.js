1c050218817fdc0bbe161e033bf022d5bd6e0093
SiddhantChadha <sid.01chadha@gmail.com>
2022-04-17 15:57:46 +0530

    added views

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const { emit } = require('process');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
 const map1 = new Map();
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    var query = socket.handshake.query;
-    var roomId = query.roomId;
+    let query = socket.handshake.query;
+    let roomId = query.roomId;
+
     socket.join(roomId);
     if (map1.has(roomId)) {
         io.to(socket.id).emit('server data emit', map1.get(roomId));
@@ -21,7 +23,7 @@ io.on('connection', (socket) => {
 
 
     socket.on('screen_state', (data, roomId) => {
-        map1.set(roomId,data);
+        map1.set(roomId, data);
         socket.to(roomId).emit('server data emit', data);
     })
 
@@ -31,6 +33,25 @@ io.on('connection', (socket) => {
 
 });
 
+
+app.post('/', function (req, res) {
+    console.log(req.body);
+
+
+    //if validated
+    
+    let details = {
+        'name': req.body.name,
+        'roomId': req.body.roomId,
+    }
+
+    res.render('board', details);
+})
+
+
+
+app.use(express.static(__dirname + '/public'));
+
 server.listen('3000', function () {
-    console.log("hello");
+    console.log("Server started on port 3000");
 })
---
+++ public/js/script.js
@@ -0,0 +1,193 @@
+
+const socket = io({
+    query: {
+        roomId: roomId,
+    },
+});
+
+console.log(nameOfUser, roomId);
+let canvas = document.querySelector("#board");
+let ctx = canvas.getContext('2d');
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+socket.on('server data emit', function (data) {
+    loadBoard(data);
+});
+
+function loadBoard(restore_state) {
+    console.log(restore_state)
+    let img = document.createElement('img');
+    img.setAttribute('src', restore_state);
+    img.onload = function () {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0);
+    }
+}
+let initialXPosition;
+let initialYPosition;
+let drawing = false;
+
+let penTool = document.querySelector("#pen");
+let eraserTool = document.querySelector("#eraser");
+let highlighterTool = document.querySelector("#highlighter");
+let undoTool = document.querySelector("#undo");
+let redoTool = document.querySelector("#redo");
+
+let download = document.querySelector("#download");
+let reset = document.querySelector("#reset");
+let bucket = document.querySelector("#bucket");
+let sizeSlider = document.querySelector("#size");
+
+let mode;
+let color = '#000000';
+let size = 5;
+
+sizeSlider.addEventListener("input", function (e) {
+    size = e.target.value;
+})
+
+reset.addEventListener("click", function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    redo_list = [];
+    undo_list = [];
+    saveState();
+
+})
+
+bucket.addEventListener("input", function (e) {
+    color = e.target.value;
+})
+
+penTool.addEventListener("click", function () {
+    mode = "pen";
+})
+
+eraserTool.addEventListener("click", function () {
+    mode = "eraser";
+})
+
+highlighterTool.addEventListener("click", function () {
+    mode = "highlighter";
+})
+
+undoTool.addEventListener("click", undo);
+redoTool.addEventListener("click", redo);
+
+download.addEventListener("click", function () {
+    let imgData = canvas.toDataURL();
+    let img = document.createElement('a');
+    img.setAttribute('href', imgData);
+    img.setAttribute('download', "download.jpeg");
+    img.click();
+})
+
+let xdeviation = canvas.offsetLeft;
+let ydeviation = canvas.offsetTop;
+let redo_list = [];
+let undo_list = [];
+saveState();
+function saveState() {
+    let imageData = canvas.toDataURL();
+    undo_list.push(imageData);
+    if (name === "admin") //dummy admin mode (to use set name admin)
+        socket.emit("screen_state", imageData, roomId);
+
+}
+
+function undo() {
+    if (undo_list.length == 1) {
+        return;
+    }
+    var redo_state = undo_list.pop();
+    var restore_state = undo_list[undo_list.length - 1];
+    loadBoard(restore_state);
+    if (name === "admin") //dummy admin mode (to use set name admin)
+        socket.emit("screen_state", restore_state, roomId);
+    redo_list.push(redo_state);
+
+}
+function redo() {
+
+    if (redo_list.length) {
+        var restore_state = redo_list.pop();
+        loadBoard(restore_state);
+        if (name === "admin") //dummy admin mode (to use set name admin)
+            socket.emit("screen_state", restore_state, roomId);
+        undo_list.push(restore_state);
+    }
+
+}
+
+let mousemove = false;
+let mouseup = true;
+canvas.addEventListener("mousedown", function (e) {
+
+    initialXPosition = e.clientX - xdeviation;
+    initialYPosition = e.clientY - ydeviation;
+
+
+    ctx.beginPath();
+    ctx.moveTo(initialXPosition, initialYPosition);
+
+    drawing = true;
+
+})
+
+
+canvas.addEventListener("mousemove", function (e) {
+
+    let finalX = e.clientX - xdeviation;
+    let finalY = e.clientY - ydeviation;
+
+    if (drawing) {
+        if (mode === "pen") {
+            mousemove = true;
+            ctx.strokeStyle = color;
+            ctx.globalCompositeOperation = "source-over";
+            ctx.lineWidth = size;
+            ctx.lineTo(finalX, finalY);
+            ctx.stroke();
+        } else if (mode === "eraser") {
+            mousemove = true;
+            ctx.strokeStyle = "#ffffff";
+            ctx.lineWidth = size;
+            ctx.globalCompositeOperation = "source-atop"; //if we don't chnage this to source-over then everything is drawn on sourceatop mode
+            ctx.lineTo(finalX, finalY);
+            ctx.stroke();
+        } else if (mode === "highlighter") {
+            mousemove = true;
+            ctx.strokeStyle = "#ff0";
+            ctx.lineWidth = size;
+            ctx.globalCompositeOperation = "multiply";
+            ctx.lineTo(finalX, finalY);
+            ctx.stroke();
+        }
+    }
+    ctx.globalCompositeOperation = "source-over";
+
+})
+
+canvas.addEventListener("mouseup", function (e) {
+    if (mousemove) {
+        mouseup = true;
+        mousemove = false;
+        redo_list = [];
+        saveState();
+    }
+    mousemove = false;
+    drawing = false;
+})
+
+
+window.addEventListener("keydown", function (e) {
+
+    if (e.ctrlKey && e.key === 'z') {
+        undo();
+    }
+    if (e.ctrlKey && e.key === 'y') {
+        redo();
+    }
+
+})
