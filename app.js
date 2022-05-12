a1af0b3969708279996fee3d266efb577f134269
RitikPandey1 <rp18.pandey@gmail.com>
2022-05-12 20:09:44 +0530

    code refactor

--- app.js
+++ app.js
@@ -58,3 +58,6 @@ app.post('/', function (req, res) {
 
 app.use(express.static(__dirname + '/public'));
 
+server.listen(process.env.PORT ||'3000', function () {
+    console.log("Server started on port 3000");
+})
