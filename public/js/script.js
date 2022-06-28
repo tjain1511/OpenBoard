f515695f206292dacb6db9860482f0b93d742e79
Tushar Jain <tjain210@gmail.com>
2022-06-28 17:27:24 +0530

    ui

--- public/js/script.js
+++ public/js/script.js
@@ -221,3 +221,23 @@ window.addEventListener("keydown", function (e) {
     }
 
 })
+let chatBtn = document.getElementById('chat-btn');
+let chatBox = document.getElementById('chat-area');
+let videoArea = document.getElementById('video-list');
+let videoBtn = document.getElementById('group-video');
+let callEnd = document.getElementById('call-end');
+
+
+callEnd.addEventListener('click',function(){
+    console.log("end call btn clicked");
+})
+
+chatBtn.addEventListener('click',function(){
+    videoArea.style.display = 'none';
+    chatBox.style.display = 'inline-block';
+})
+
+videoBtn.addEventListener('click',function(){
+    chatBox.style.display = 'none';
+    videoArea.style.display = 'inline-block';
+})
